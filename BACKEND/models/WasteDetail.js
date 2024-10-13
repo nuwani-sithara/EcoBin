4cdd591b2797c42b379c7c5cf29805d77d68483b
nuwani-sithara <nuwanisithara.com@gmail.com>
2024-10-13 20:32:24 +0530

    added new files to the frontend

--- BACKEND/models/WasteDetail.js
+++ BACKEND/models/WasteDetail.js
@@ -1,32 +1,31 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
 
 const wasteSchema = new Schema({
-
-    category : {
-        type : String,
-        required : true
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
     },
-    waste : {
-        type : String,
-        required : true
+    waste: {
+        type: String,
+        required: true
     },
-    weight : {
-        type : Number,
-        required : true
+    weight: {
+        type: Number,
+        required: true
     },
-    weightType : {
-        type : String,
-        required : true
+    weightType: {
+        type: String,
+        required: true
     },
-    quantity : {
-        type : Number,
-        required : true
+    quantity: {
+        type: Number,
+        required: true
     }
-})
+}, {
+    timestamps: true
+});
 
 const WasteDetail = mongoose.model("WasteDetail", wasteSchema);
-
-
 module.exports = WasteDetail;
